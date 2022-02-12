7c8add3b5593e967a69c8f7567bbd3dafebb9174
Slide12 <77026354+Slide12@users.noreply.github.com>
2022-02-11 20:42:22 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ titles = [
 
 tokenname = f"{adjectives[random.randint(0, len(adjectives)-1)]} {titles[random.randint(0, len(titles)-1)]}"
 
-photo = Image.open('back.png')
+photo = Image.open('back.jpg')
 draw = ImageDraw.Draw(photo)
 width, height = photo.size
 
@@ -23,31 +23,70 @@ def randcolor():
   b = random.randint(50, 200)
   return r, g, b
 
-def randcolorover():
-  r = random.randint(0, 255)
-  g = random.randint(0, 255)
-  b = random.randint(250, 255)
-  return r, g, b
+# Filter Selector
+artfilter = random.randint(1, 5)
+if artfilter > 5: #20% chance of Monochrome
+  artfilter = 'col'
+  colrange = 4
+else:
+  artfilter = 'mono' #80% chance of Color
+  colrange = 3
 
+# --- Scheme Selector --- #
+cols = [(115, 38, 38), (51, 23, 87), (212, 219, 66), (96, 147, 189), (48, 48, 48)] 
+# Red: 0, Purple: 1, Yellow: 2, Blue: 3, Gray: 4
+
+monos = [(0, 0, 0), (255, 255, 255), (33, 33, 33), (112, 112, 112)]
+# Black: 0, White: 1, Dark Gray: 2, Light Gray: 3
+
+rerollspecialty = 0
+firstcolsel = random.randint(0, colrange)
+secondcolsel = random.randint(0, colrange)
+# Reroll Specialty grants special attribuite depending on the number of rerolls.
+
+while True:
+  if firstcolsel == secondcolsel:
+   secondcolsel = random.randint(0, colrange)
+   rerollspecialty += 1 
+  else:
+    break
+
+if artfilter == 'col':
+  firstcol = cols[firstcolsel]
+  secondcolsel = cols[secondcolsel]
+elif artfilter == 'mono':
+  firstcol = monos[firstcolsel]
+  secondcol= monos[secondcolsel]
+
+def schemealt():
+  c = random.randint(0, 1)
+  if c == 0:
+    return firstcol
+  elif c == 1:
+    return secondcol
+# --- Scheme Selector End --- #
+
+# -- Trait Selector --- #
+
+
+draw.rectangle((0, 0, width, height), fill=(255, 255, 255))
 draw.rectangle((0, 0, width, height), fill=(0, 0, 0))
-draw.rectangle((0, 0, width, height), fill=randcolor())
 photo.save('back.jpg')
 
 count = 1
 shapes = 60
-top_images = 0
 
 def glitchbranch():
   branch_count = random.randint(5, 15)
   for _ in range(branch_count):
-    fillcol = randcolor()
+    fillcol = schemealt()
     branch_pos = random.randint(10, 500)
-    draw.rectangle((0, branch_pos, width, height), outline=randcolor())
+    draw.rectangle((0, branch_pos, width, height), outline=schemealt())
 
 def shards():
   shard_count = random.randint(0, 5)
   for _ in range(shard_count):
-    fillcol = randcolor()
+    fillcol = schemealt()
     x_1 = random.randint(0, width)
     y_1 = random.randint(0, height)
     x_2 = x_1 + random.randint(5, 20)
@@ -57,20 +96,11 @@ def shards():
 def censorship():
   censor_count = random.randint(0, 5)
   for _ in range(censor_count):
-    fillcol = 0
     x_1 = random.randint(0, width)
     y_1 = random.randint(0, height)
     x_2 = x_1 + random.randint(200, 300)
     y_2 = y_1 + random.randint(200, 300)
-    draw.line((x_1, y_1, x_2, y_1), width=random.randint(40, 80), fill=fillcol)
-
-def randshapes():
-  x_1 = random.randint(0, width - 10)
-  y_1 = random.randint(x_1, height - 10)
-  x_2 = random.randint(y_1, width - 10)
-  y_2 = random.randint(x_2, height - 10)
-  shape_width = 5
-  draw.rectangle ((x_1, y_1, x_2, y_2), fill=randcolor(), width=shape_width, outline=0)
+    draw.line((x_1, y_1, x_2, y_1), width=random.randint(40, 80), fill=0)
 
 def shape_drag():
   draglen = random.randint(3, 10)
@@ -84,9 +114,9 @@ def shape_drag():
 
   for f in range(draglen):
     if f < 1:
-      draw.rectangle((start_x, start_y, start_x + size, start_y - size), fill=randcolor(), outline=0, width=5)
+      draw.rectangle((start_x, start_y, start_x + size, start_y - size), fill=schemealt(), outline=0, width=5)
     else:
-      draw.rectangle((start_x, start_y, start_x + size, start_y - size), fill=randcolor(), outline=0, width=5)
+      draw.rectangle((start_x, start_y, start_x + size, start_y - size), fill=schemealt(), outline=0, width=5)
       start_x += x_dir
       start_y -= y_dir
 
@@ -97,9 +127,10 @@ for f in range(count):
   for _ in range(shapes):
     shape_drag()
     glitchbranch()
-    shards()
 
   shape_drag()
+  censorship()
   photo.save(f'nft{f + 1}.png')
   print(width, height)
+  print(rerollspecialty, firstcol, secondcol)
   print(tokenname)
